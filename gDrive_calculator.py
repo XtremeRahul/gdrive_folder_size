699f13a07f4285a846352f45686ed3952e70f055
XtremeRahul <66631560+XtremeRahul@users.noreply.github.com>
2020-08-29 21:04:35 +0530

    Update gDrive_calculator.py

--- gDrive_calculator.py
+++ gDrive_calculator.py
@@ -12,137 +12,137 @@ from googleapiclient.discovery import build
 
 
 def get_readable_file_size(size_in_bytes) -> str:
-	SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-	if size_in_bytes is None:
-		return '0B'
-	index = 0
-	while size_in_bytes >= 1024:
-		size_in_bytes /= 1024
-		index += 1
-	try:
-		return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
-	except IndexError:
-		return 'File too large'
+    SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
 
 
 class GoogleDriveSizeCalculate:
-	def __init__(self, service=None):
-		self.__G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-		self.__OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive']
-		self.__service = service
-		if service is None:
-			print("Pass service to GoogleDriveSizeCalculate class.")
-			return
-		self.main_name = None
-		self.total_bytes = 0
-		self.total_files = 0
-		self.total_folders = 0
+    def __init__(self, service=None):
+        self.__G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
+        self.__OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive']
+        self.__service = service
+        if service is None:
+            print("Pass service to GoogleDriveSizeCalculate class.")
+            return
+        self.main_name = None
+        self.total_bytes = 0
+        self.total_files = 0
+        self.total_folders = 0
 
-	@staticmethod
-	def getIdFromUrl(link: str):
-		if "folders" in link or "file" in link:
-			regex = r"https://drive\.google\.com/(drive)?/?u?/?\d?/?(mobile)?/?(file)?(folders)?/?d?/([-\w]+)[?+]?/?(w+)?"
-			res = re.search(regex,link)
-			if res is None:
-				return "GDrive ID not found. Try sending url in different format."
-			return res.group(5)
-		parsed = urlparse.urlparse(link)
-		return parse_qs(parsed.query)['id'][0]
+    @staticmethod
+    def getIdFromUrl(link: str):
+        if "folders" in link or "file" in link:
+            regex = r"https://drive\.google\.com/(drive)?/?u?/?\d?/?(mobile)?/?(file)?(folders)?/?d?/([-\w]+)[?+]?/?(w+)?"
+            res = re.search(regex,link)
+            if res is None:
+                return "GDrive ID not found. Try sending url in different format."
+            return res.group(5)
+        parsed = urlparse.urlparse(link)
+        return parse_qs(parsed.query)['id'][0]
 
-	def gdrive_checker(self, LINKorID):
-		if self.__service is None:
-			return
-		if 'drive.google.com' in LINKorID:
-			try:
-				file_id = self.getIdFromUrl(LINKorID)
-				if 'GDrive ID not found.' in file_id:
-					print(file_id)
-					return
-			except (KeyError, IndexError):
-				msg = f"GDrive ID could not be found in the provided link."
-				print(msg)
-				return
-		else:
-			file_id = LINKorID.strip()
+    def gdrive_checker(self, LINKorID):
+        if self.__service is None:
+            return
+        if 'drive.google.com' in LINKorID:
+            try:
+                file_id = self.getIdFromUrl(LINKorID)
+                if 'GDrive ID not found.' in file_id:
+                    print(file_id)
+                    return
+            except (KeyError, IndexError):
+                msg = f"GDrive ID could not be found in the provided link."
+                print(msg)
+                return
+        else:
+            file_id = LINKorID.strip()
 
-		error = False
-		print("Calculating... Please Wait!")
+        error = False
+        print("Calculating... Please Wait!")
 
-		try:
-			drive_file = self.__service.files().get(fileId=file_id, fields="id, name, mimeType, size",
-												   supportsTeamDrives=True).execute()
-			self.main_name = drive_file['name']
-			if drive_file['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
-				self.total_folders += 1 
-				self.gDrive_directory(**drive_file)
-			else:
-				self.total_files += 1
-				self.gDrive_file(**drive_file)
+        try:
+            drive_file = self.__service.files().get(fileId=file_id, fields="id, name, mimeType, size",
+                                                   supportsTeamDrives=True).execute()
+            self.main_name = drive_file['name']
+            if drive_file['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
+                self.total_folders += 1 
+                self.gDrive_directory(**drive_file)
+            else:
+                self.total_files += 1
+                self.gDrive_file(**drive_file)
 
-		except Exception as e:
-			print('\n')
-			if 'HttpError' in str(e):
-				h_e = e
-				ori = str(h_e)
-				try:
-					h_e = ori
-					h_e = h_e.replace('<', '').replace('>', '')
-					h_e = h_e.split('when')
-					f = h_e[0].strip()
-					s = h_e[1].split('"')[1].split('"')[0].strip()
-					e =  f"{f}\n{s}"
-				except:
-					e = ori
-			print(str(e))
-			error = True
-		finally:
-			if error:
-				return
-			return {
-				'name': self.main_name, 
-				'size': get_readable_file_size(self.total_bytes), 
-				'bytes': self.total_bytes,
-				'files': self.total_files,
-				'folders': self.total_folders
-				}
+        except Exception as e:
+            print('\n')
+            if 'HttpError' in str(e):
+                h_e = e
+                ori = str(h_e)
+                try:
+                    h_e = ori
+                    h_e = h_e.replace('<', '').replace('>', '')
+                    h_e = h_e.split('when')
+                    f = h_e[0].strip()
+                    s = h_e[1].split('"')[1].split('"')[0].strip()
+                    e =  f"{f}\n{s}"
+                except:
+                    e = ori
+            print(str(e))
+            error = True
+        finally:
+            if error:
+                return
+            return {
+                'name': self.main_name, 
+                'size': get_readable_file_size(self.total_bytes), 
+                'bytes': self.total_bytes,
+                'files': self.total_files,
+                'folders': self.total_folders
+                }
 
-	def list_drive_dir(self, file_id: str) -> list:
-		query = f"'{file_id}' in parents and (name contains '*')"
-		fields = 'nextPageToken, files(id, mimeType, size)'
-		page_token = None
-		page_size = 1000
-		files = []
-		while True:
-			response = self.__service.files().list(supportsTeamDrives=True,
-												  includeTeamDriveItems=True,
-												  q=query, spaces='drive',
-												  fields=fields, pageToken=page_token,
-												  pageSize=page_size, corpora='allDrives',
-												  orderBy='folder, name').execute()
-			files.extend(response.get('files', []))
-			page_token = response.get('nextPageToken', None)
-			if page_token is None:
-				break
-		return files
+    def list_drive_dir(self, file_id: str) -> list:
+        query = f"'{file_id}' in parents and (name contains '*')"
+        fields = 'nextPageToken, files(id, mimeType, size)'
+        page_token = None
+        page_size = 1000
+        files = []
+        while True:
+            response = self.__service.files().list(supportsTeamDrives=True,
+                                                  includeTeamDriveItems=True,
+                                                  q=query, spaces='drive',
+                                                  fields=fields, pageToken=page_token,
+                                                  pageSize=page_size, corpora='allDrives',
+                                                  orderBy='folder, name').execute()
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return files
 
-	def gDrive_file(self, **kwargs):
-		try:
-			size = int(kwargs['size'])
-		except:
-			size = 0
-		self.total_bytes += size
+    def gDrive_file(self, **kwargs):
+        try:
+            size = int(kwargs['size'])
+        except:
+            size = 0
+        self.total_bytes += size
 
-	def gDrive_directory(self, **kwargs) -> None:
-		files = self.list_drive_dir(kwargs['id'])
-		if len(files) == 0:
-			return
-		for file_ in files:
-			if file_['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
-				self.total_folders += 1
-				self.gDrive_directory(**file_)
-			else:
-				self.total_files += 1
-				self.gDrive_file(**file_)
+    def gDrive_directory(self, **kwargs) -> None:
+        files = self.list_drive_dir(kwargs['id'])
+        if len(files) == 0:
+            return
+        for file_ in files:
+            if file_['mimeType'] == self.__G_DRIVE_DIR_MIME_TYPE:
+                self.total_folders += 1
+                self.gDrive_directory(**file_)
+            else:
+                self.total_files += 1
+                self.gDrive_file(**file_)
 
 
 
@@ -188,9 +188,9 @@ calculate = calculator.gdrive_checker(LINKorID)
 ## Note that, gdrive folder size calculating speed depends on how many file inside a folder.
 
 if not calculate is None:
-	print('\n')
-	for k, v in calculate.items():
-		print(f'{k.title()}:', v)
+    print('\n')
+    for k, v in calculate.items():
+        print(f'{k.title()}:', v)
 
 
 """
